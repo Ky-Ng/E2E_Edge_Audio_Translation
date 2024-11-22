b35b9f78ed3bf904d8625d123f5c86b88083588c
OjDwagon <ongjd@usc.edu>
2024-11-21 20:31:19 -0800

    Implemented main e2e_client_server driver and updated BluetoothHandler object. Created file of callbacks as placeholders for later integration.

--- e2e/utils/BluetoothHandler.py
+++ e2e/utils/BluetoothHandler.py
@@ -1,22 +1,31 @@
 from bluedot.btcomm import BluetoothClient
+from bluedot.btcomm import BluetoothServer
 from signal import pause
 
 
 class BluetoothHandler():
-    MINDY_ADDR = "D8:3A:DD:B5:C2:EB"
-    def __init__(self) -> None:
-        self.c = BluetoothClient("D8:3A:DD:B5:C2:EB", self.on_data_received)
+    # MINDY_ADDR = "D8:3A:DD:B5:C2:EB"
+    # Constructor for server
+    def __init__(self, receivedCallbacks, sendingCallback) -> None:
+        self.c = BluetoothServer(self.on_data_received)
+        self.receivedCallbacks = receivedCallbacks
+        self.sendingCallback = sendingCallback
 
+    # Constructor for client
+    def __init__(self, receivedCallbacks, sendingCallback, serverMacAddress) -> None:
+        self.c = BluetoothClient(serverMacAddress, self.on_data_received)
+        self.receivedCallbacks = receivedCallbacks
+        self.sendingCallback = sendingCallback
+
+    # On data receipt, pass through all call backs
     def on_data_received(self, data):
-        return data
-    
-    
-    
+        for callback in self.receivedCallbacks:
+            callback(data)
+
     def send(self, msg:str) -> None:
         self.c.send(msg)
 
-
-# msg = input("enter a message: ")
-# while msg != '':
-
-#     msg = input("enter a message: ")
+    # Infinite loop of sending to partner
+    def start(self) -> None:
+        while True:
+            self.send(self.sendingCallback())
