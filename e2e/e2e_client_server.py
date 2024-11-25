b5281d89fb68f1d0f67ec139904ea5f54ed38cdb
Kyle N <kgng@usc.edu>
2024-11-25 11:46:36 -0800

    feat/unit-testing (#5)
    * unit tests for asr and audio handling
    
    * tts checking
    
    * move bluetooth to library
    
    * working self loop pipeline
    
    * ready to test next e2e with bluetooth
    
    * ready to test callbacks
    
    * remove unused files
    
    * add choices to self test loop

--- e2e/e2e_client_server.py
+++ e2e/e2e_client_server.py
@@ -16,18 +16,18 @@ def main():
 
     # Create callbacks based on language
     if args.nativeLanguage == 'en':
-        sendingCallback = utils.CallBacks.getInputFromEnglish
-        receivingCallback = [utils.CallBacks.outputInputEnglish]
+        sendingCallback = utils.CallBacks.get_input_to_english
+        receivingCallback = [utils.CallBacks.synthesize_speech_english]
     elif args.nativeLanguage == 'ch':
         sendingCallback = utils.CallBacks.getInputFromChinese
         receivingCallback = [utils.CallBacks.outputInputChinese]
     else:
         sendingCallback = utils.CallBacks.getInputFromTerminal
-        receivingCallback = [utils.CallBacks.printInput]
+        receivingCallback = [utils.CallBacks.print_input]
 
     # Add print callback if verbose
     if args.verbose and args.nativeLanguage != 'terminal':
-        receivingCallback.append(utils.CallBacks.printInput)
+        receivingCallback.append(utils.CallBacks.print_input)
 
     # Create client or server
     if args.serverMac is None: # Server
